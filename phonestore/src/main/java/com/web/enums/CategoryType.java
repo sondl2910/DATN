e07ea1003bb25ece158af4ab76930607a5de0c69
sondl2910 <ledinhson291003@gmail.com>
2025-03-31 00:34:26 +0700

    update

---
+++ phonestore/src/main/java/com/web/enums/CategoryType.java
@@ -0,0 +1,11 @@
+package com.web.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CategoryType {
+
+    DIEN_THOAI, PHU_KIEN
+}
---
+++ phonestore/src/main/java/com/web/enums/StatusInvoice.java
@@ -0,0 +1,12 @@
+package com.web.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum StatusInvoice {
+
+    DANG_CHO_XAC_NHAN,
+    DA_XAC_NHAN, DA_GUI, DA_NHAN, DA_HUY, KHONG_NHAN_HANG;
+}
