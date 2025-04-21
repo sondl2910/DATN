dc11f40bc93178c477be69fbed0116277a905ab2
sondl2910 <ledinhson291003@gmail.com>
2025-04-21 22:05:01 +0700

    update

---
+++ phonestore/src/main/java/com/web/api/MomoApi.java
@@ -0,0 +1,69 @@
+package com.web.api;
+import com.web.config.Environment;
+import com.web.constants.LogUtils;
+import com.web.constants.RequestType;
+import com.web.dto.request.PaymentDto;
+import com.web.dto.response.ResponsePayment;
+import com.web.entity.Voucher;
+import com.web.exception.MessageException;
+import com.web.models.PaymentResponse;
+import com.web.models.QueryStatusTransactionResponse;
+import com.web.processor.CreateOrderMoMo;
+import com.web.processor.QueryTransactionStatus;
+import com.web.servive.CartService;
+import com.web.servive.VoucherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class MomoApi {
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private VoucherService voucherService;
+
+    @PostMapping("/urlpayment")
+    public ResponsePayment getUrlPayment(@RequestBody PaymentDto paymentDto){
+        LogUtils.init();
+        Double totalAmount = cartService.totalAmountCart();
+        totalAmount += paymentDto.getShipCost();
+        if(paymentDto.getCodeVoucher() != null){
+            Optional<Voucher> voucher = voucherService.findByCode(paymentDto.getCodeVoucher(), totalAmount);
+            if(voucher.isPresent()){
+                totalAmount = totalAmount - voucher.get().getDiscount();
+            }
+        }
+
+        Long td = Math.round(totalAmount);
+        String orderId = String.valueOf(System.currentTimeMillis());
+        String requestId = String.valueOf(System.currentTimeMillis());
+        Environment environment = Environment.selectEnv("dev");
+        PaymentResponse captureATMMoMoResponse = null;
+        try {
+            captureATMMoMoResponse = CreateOrderMoMo.process(environment, orderId, requestId, Long.toString(td), paymentDto.getContent(), paymentDto.getReturnUrl(), paymentDto.getNotifyUrl(), "", RequestType.PAY_WITH_ATM, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("url ====: "+captureATMMoMoResponse.getPayUrl());
+        ResponsePayment responsePayment = new ResponsePayment(captureATMMoMoResponse.getPayUrl(),orderId,requestId);
+        return responsePayment;
+    }
+
+
+    @GetMapping("/checkPayment")
+    public Integer checkPayment(@RequestParam("orderId") String orderId, @RequestParam("requestId") String requestId) throws Exception {
+        Environment environment = Environment.selectEnv("dev");
+        QueryStatusTransactionResponse queryStatusTransactionResponse = QueryTransactionStatus.process(environment, orderId, requestId);
+        System.out.println("qqqq-----------------------------------------------------------"+queryStatusTransactionResponse.getMessage());
+        if(queryStatusTransactionResponse.getResultCode() == 0){
+            return 0;
+        }
+        return 1;
+    }
+}
---
+++ phonestore/src/main/java/com/web/api/StatiticsApi.java
@@ -0,0 +1,77 @@
+package com.web.api;
+
+import com.web.enums.StatusInvoice;
+import com.web.repository.InvoiceRepository;
+import com.web.repository.ProductRepository;
+import com.web.repository.UserRepository;
+import com.web.utils.Contains;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/statistic")
+@CrossOrigin
+public class StatiticsApi {
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/admin/revenue-this-month")
+    public Double doanhThuThangNay(){
+        Date date = new Date(System.currentTimeMillis());
+        String[] str = date.toString().split("-");
+        Integer year = Integer.valueOf(str[0]);
+        Integer month = Integer.valueOf(str[1]);
+        int index = Arrays.asList(StatusInvoice.values()).indexOf(StatusInvoice.DA_NHAN);
+        return invoiceRepository.calDt(month, year, index);
+    }
+
+    @GetMapping("/admin/revenue-today")
+    public Double revenueByDate(){
+        Date date = new Date(System.currentTimeMillis());
+        int index = Arrays.asList(StatusInvoice.values()).indexOf(StatusInvoice.DA_NHAN);
+        return invoiceRepository.revenueByDate(date,index);
+    }
+
+    @GetMapping("/admin/number-invoice-today-finish")
+    public Double numInvoiceToDay(){
+        Date date = new Date(System.currentTimeMillis());
+        int index = Arrays.asList(StatusInvoice.values()).indexOf(StatusInvoice.DA_NHAN);
+        return invoiceRepository.numInvoiceToDay(date, index);
+    }
+
+    @GetMapping("/admin/number-admin")
+    public Double numberAdmin(){
+        return userRepository.countAdmin(Contains.ROLE_ADMIN);
+    }
+
+    @GetMapping("/admin/number-product")
+    public Long numberProduct(){
+        return productRepository.count();
+    }
+
+    @GetMapping("/admin/revenue-year")
+    public List<Double> doanhThu(@RequestParam("year") Integer year){
+        List<Double> list = new ArrayList<>();
+        int index = Arrays.asList(StatusInvoice.values()).indexOf(StatusInvoice.DA_NHAN);
+        for(int i=1; i< 13; i++){
+            Double sum = invoiceRepository.calDt(i, year, index);
+            if(sum == null){
+                sum = 0D;
+            }
+            list.add(sum);
+        }
+        return list;
+    }
+}
